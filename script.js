eafa04c488a0406f76d42b96507c86ad8919b667
paunpaun <abc795651@gmail.com>
2024-09-12 17:06:53 +0200

    commiut

--- script.js
+++ script.js
@@ -10,6 +10,55 @@ function f(id){
             img.src ='assets/cross.png'
         }
     
-        circle_turn = !circle_turn   
+        circle_turn = !circle_turn 
+        
+        check_tris()
+    }
+}
+
+function check_tris(){
+    var list = ['circle','cross']
+
+    for (element of list){
+        
+        if( 
+            //diagonals
+            (document.getElementById('box-1').src.includes(element) &&
+            document.getElementById('box-5').src.includes(element) &&
+            document.getElementById('box-9').src.includes(element)) ||
+
+            (document.getElementById('box-3').src.includes(element) &&
+            document.getElementById('box-5').src.includes(element) &&
+            document.getElementById('box-7').src.includes(element)) ||
+
+            //rows
+            (document.getElementById('box-1').src.includes(element) &&
+            document.getElementById('box-2').src.includes(element) &&
+            document.getElementById('box-3').src.includes(element)) ||
+
+            (document.getElementById('box-4').src.includes(element) &&
+            document.getElementById('box-5').src.includes(element) &&
+            document.getElementById('box-6').src.includes(element)) ||
+
+            (document.getElementById('box-7').src.includes(element) &&
+            document.getElementById('box-8').src.includes(element) &&
+            document.getElementById('box-9').src.includes(element)) ||
+            
+            //columns
+            (document.getElementById('box-1').src.includes(element) &&
+            document.getElementById('box-4').src.includes(element) &&
+            document.getElementById('box-7').src.includes(element)) ||
+
+            (document.getElementById('box-2').src.includes(element) &&
+            document.getElementById('box-5').src.includes(element) &&
+            document.getElementById('box-8').src.includes(element)) ||
+
+            (document.getElementById('box-3').src.includes(element) &&
+            document.getElementById('box-6').src.includes(element) &&
+            document.getElementById('box-9').src.includes(element))
+        ){
+            alert(element + ' wins')
+            window.location.reload();
+        }
     }
 }
